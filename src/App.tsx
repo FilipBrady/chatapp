9c79253ddc467a30a0a73378de70efb9fae14652
Filip B <filipbrady@gmail.com>
2023-02-13 20:40:02 +0100

    Done--without styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/firestore';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/analytics';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import {
+//   useCollection,
+//   useCollectionData,
+// } from 'react-firebase-hooks/firestore';
+
+// import { getFirestore } from 'firebase/firestore'
+// import { getAuth, signInWithPopup } from 'firebase/auth';
+// import { collection } from 'firebase/firestore';
+// import { initializeApp } from 'firebase/app';
+// import React, { useRef, useState, useEffect} from 'react';
+// import './App.css';	import './App.css';
+
+// import firebase from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
+// import 'firebase/firestore';
+// import {
+//   getFirestore,
+//   collection,
+//   query,
+//   orderBy,
+//   limit,
+//   serverTimestamp,
+//   addDoc,
+// } from 'firebase/firestore';
+// import 'firebase/auth';
+// import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+// import 'firebase/analytics'; //import 'firebase/compat/analytics';
+
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import { useCollectionData } from 'react-firebase-hooks/firestore';
+// import { useState } from 'react';
+
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-import 'firebase/analytics';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
 
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
-import { getAuth, signInWithPopup } from 'firebase/auth';
+import {
+  getFirestore,
+  collection,
+  query,
+  orderBy,
+  limit,
+  serverTimestamp,
+  addDoc,
+} from 'firebase/firestore';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+//import 'firebase/compat/analytics';
 
-import { Query } from '@firebase/firestore';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 const firebaseConfig = {
   apiKey: 'AIzaSyAzD287l6JGtJ5Kjlo_H6nL0SZjFQQyS6M',
   authDomain: 'chatapp-d8ad3.firebaseapp.com',
@@ -21,58 +63,113 @@ const firebaseConfig = {
   messagingSenderId: '28905240343',
   appId: '1:28905240343:web:7e452b122dec1f1c876952',
 };
-const app = firebase.initializeApp(firebaseConfig);
-const db = getFirestore(app);
-// const auth = firebase.auth();
-const auth = getAuth(app);
-const firestore = firebase.firestore();
-// const analytics = firebase.analytics();
+initializeApp(firebaseConfig);
+// init firestore services
+const db = getFirestore();
+const auth = getAuth();
+//const analytics = firebase.analytics();
+const uid = auth.currentUser?.uid;
+const photoURL = auth.currentUser?.photoURL;
 function App() {
   const [user] = useAuthState(auth);
+
   return (
     <div className='App'>
       <h1>
         <SignOut />
       </h1>
-      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+      <section>{user ? <ChatRoom user={user} /> : <SignIn />}</section>
     </div>
   );
 }
 
 function SignIn() {
   const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
   };
   return <button onClick={signInWithGoogle}>Sign in with Google </button>;
 }
+// function SignOut() {
+//   return auth.currentUser?.uid !== undefined ? (
+//     <button onClick={() => auth.signOut()}>Sign Out</button>
+//   ) : (
+//     <div></div>
+//   );
+// }
 function SignOut() {
   return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+    auth.currentUser && (
+      <button className='sign-out' onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
+function ChatRoom(props: any) {
+  // const messagesRef = firestore.collection('messages');
+  // const query = messagesRef.orderBy('createdAt');
+
+  // const [messages] = useCollectionData(query, { idField: 'id' });
+  // const [messages] = useCollectionData(collection(db, 'messages'), {
+  //   snapshotListenOptions: { includeMetadataChanges: true },
+  // });
+  // const messagesRef = firestore.collection("messages")
+  // const q = (messagesRef.orderBy("createdAt"), messagesRef.limit(25))
+  // const [messages] = useCollectionData(q, { idField: "id" })
+  const { user } = props.user;
+  const messagesRef = collection(db, 'messages');
+  const q = query(messagesRef, orderBy('createdAt', 'asc'), limit(25));
+
+  const [messages] = useCollectionData(q);
+
+  // const dummy = useRef();
+  // const messageInput = useRef();
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e: any) => {
+    e.preventDefault();
+    // const uid = auth.currentUser?.uid;
+    // const photoURL = auth.currentUser?.photoURL;
+
+    // const { uid, photoURL } = auth.currentUser;
+    if (formValue !== null) {
+      await addDoc(messagesRef, {
+        uid,
+        photoURL,
+        text: formValue || null,
+        createdAt: serverTimestamp(),
+      });
+      // messageInput.current.focus();
+      setFormValue('');
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {messages &&
+          messages.map((msg, idx) => <ChatMessage key={idx} message={msg} />)}
+      </div>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={e => setFormValue(e.target.value)} />
+        <button type='submit' disabled={!formValue}>
+          Send message
+        </button>
+      </form>
+    </div>
   );
 }
 
-function ChatRoom() {
-  const messagesRef = firestore.collectionGroup('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-  
-  const [messages] = useCollectionData(query, { idField: 'id' });
-  
-  
-  return (
-  <div>
-    <div>
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-    </div>
-  </div>)
-}
-
 function ChatMessage(props: any) {
-  const {text, uid } = props.message
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser?.uid ? 'sent' : 'recieved';
   return (
-    <div>
-      {text}
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} referrerPolicy='no-referrer' alt='User' />
+      <p>{text}</p>
     </div>
-  )
+  );
 }
 export default App;
